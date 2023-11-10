b00a2c135328cd99b9f6811fa4782a7fce6ae456
Thien Long Ngo <ngo.andrej@gmail.com>
2023-11-10 11:16:34 +0100

    Refactor logic and add modal with a random fact after all phases are completed

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { Checkbox, FormControlLabel, Typography } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import {
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 
 interface Task {
   id: number;
@@ -13,106 +22,186 @@ interface Phase {
   tasks: Task[];
 }
 
-const initialPhases: Phase[] = [
-  {
-    id: 1,
-    name: 'Phase 1',
-    tasks: [
-      { id: 1, description: 'Task 1.1', completed: false },
-      { id: 2, description: 'Task 1.2', completed: false },
-    ],
-  },
-  {
-    id: 2,
-    name: 'Phase 2',
-    tasks: [
-      { id: 3, description: 'Task 2.1', completed: false },
-      { id: 4, description: 'Task 2.2', completed: false },
-    ],
-  },
-  {
-    id: 3,
-    name: 'Phase 3',
-    tasks: [
-      { id: 5, description: 'Task 3.1', completed: false },
-      { id: 6, description: 'Task 3.2', completed: false },
-    ],
-  },
-  // Add more phases as needed
-];
+interface AppData {
+  title: string;
+  phases: Phase[];
+}
+
+const initialData: AppData = {
+  title: "My startup progress",
+  phases: [
+    {
+      id: 1,
+      name: "Foundation",
+      tasks: [
+        { id: 1, description: "Setup virtual office", completed: false },
+        { id: 2, description: "Set mission & vision", completed: false },
+        { id: 3, description: "Select business name", completed: false },
+        { id: 4, description: "Buy domains", completed: false },
+      ],
+    },
+    {
+      id: 2,
+      name: "Discovery",
+      tasks: [
+        { id: 5, description: "Create roadmap", completed: false },
+        { id: 6, description: "Competitor analysis", completed: false },
+      ],
+    },
+    {
+      id: 3,
+      name: "Delivery",
+      tasks: [
+        { id: 7, description: "Release marketing website", completed: false },
+        { id: 8, description: "Release MVP", completed: false },
+      ],
+    },
+  ],
+};
 
 const Home: React.FC = () => {
-  const [phases, setPhases] = useState(initialPhases);
-  
+  const [appData, setAppData] = useState<AppData>(initialData);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const [randomFact, setRandomFact] = useState<string | null>(null);
+
   useEffect(() => {
-    const storedPhases = localStorage.getItem('phases');
-    if (storedPhases) {
-      setPhases(JSON.parse(storedPhases));
+    const storedData = localStorage.getItem("appData");
+    if (storedData) {
+      setAppData(JSON.parse(storedData));
     }
   }, []);
-  
-  const saveToLocalStorage = () => {
-    localStorage.setItem('phases', JSON.stringify(phases));
+
+  const saveToLocalStorage = (data: AppData) => {
+    localStorage.setItem("appData", JSON.stringify(data));
   };
-  
-  const canMarkTask = (phaseId: number, taskId: number) => {
-    // Allow marking tasks in the next phase if the current phase is completed
-    const currentPhaseIndex = phases.findIndex((phase) => phase.id === phaseId);
-    if (currentPhaseIndex > 0) {
-      const previousPhase = phases[currentPhaseIndex - 1];
-      return previousPhase.tasks.every((task) => task.completed);
+
+  const canMarkTask = (phaseIndex: number, taskIndex: number) => {
+    if (phaseIndex === 0) {
+      return taskIndex < appData.phases[phaseIndex].tasks.length;
     }
-    
-    // Allow marking the first task in a phase without restrictions
-    if (taskId === 1) {
-      return true;
+
+    const currentPhase = appData.phases[phaseIndex];
+    const previousPhase = appData.phases[phaseIndex - 1];
+
+    // Check if all tasks in the previous phase are completed
+    if (!previousPhase.tasks.every((task) => task.completed)) {
+      return false;
     }
-    
-    // Check if all preceding tasks in the same phase are completed
-    const currentPhase = phases.find((phase) => phase.id === phaseId);
-    const precedingTasks = currentPhase?.tasks.filter((task) => task.id < taskId) || [];
-    return (
-      precedingTasks.every((task) => task.completed) &&
-      currentPhase?.tasks.every((task) => task.id <= taskId)
-    );
+
+    // Check if the task is within the current phase
+    return taskIndex < currentPhase.tasks.length;
   };
-  
-  const handleTaskToggle = (phaseId: number, taskId: number) => {
-    if (canMarkTask(phaseId, taskId)) {
-      const updatedPhases = phases.map((phase) => {
-        if (phase.id === phaseId) {
-          const updatedTasks = phase.tasks.map((task) =>
-            task.id === taskId ? { ...task, completed: !task.completed } : task
+
+  const handleTaskToggle = (phaseIndex: number, taskIndex: number) => {
+    const updatedData: AppData = {
+      ...appData,
+      phases: appData.phases.map((phase, idx) => {
+        if (idx === phaseIndex) {
+          const updatedTasks = phase.tasks.map((task, taskIdx) =>
+            taskIdx === taskIndex
+              ? { ...task, completed: !task.completed }
+              : task,
           );
           return { ...phase, tasks: updatedTasks };
         }
         return phase;
-      });
-      
-      setPhases(updatedPhases);
-      saveToLocalStorage();
-    } else {
-      alert('Complete all preceding tasks in the same phase first.');
+      }),
+    };
+
+    setAppData(updatedData);
+    saveToLocalStorage(updatedData);
+  };
+
+  const areAllPhasesCompleted = () => {
+    return appData.phases.every((phase) =>
+      phase.tasks.every((task) => task.completed),
+    );
+  };
+
+  const handleCompleteAllPhases = () => {
+    const updatedData: AppData = {
+      ...appData,
+      phases: appData.phases.map((phase) => ({
+        ...phase,
+        tasks: phase.tasks.map((task) => ({ ...task, completed: true })),
+      })),
+    };
+
+    setAppData(updatedData);
+    saveToLocalStorage(updatedData);
+
+    // Fetch a random fact when all phases are completed
+    if (areAllPhasesCompleted()) {
+      fetch("https://uselessfacts.jsph.pl/random.json")
+        .then((response) => response.json())
+        .then((data) => {
+          setRandomFact(data.text);
+          setOpenDialog(true);
+        })
+        .catch((error) =>
+          console.error("Failed to fetch a random fact:", error),
+        );
     }
   };
-  
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+    setRandomFact(null);
+  };
+
   return (
     <div>
-      {phases.map((phase) => (
-        <div key={phase.id}>
-          <Typography variant="h4">
-            {phase.name}
-            {phase.tasks.every((task) => task.completed) && ' ✔️'}
-          </Typography>
-          {phase.tasks.map((task) => (
+      <Typography variant="h3" style={{ marginBottom: "3rem" }}>
+        {appData.title}
+      </Typography>
+      {appData.phases.map((phase, phaseIndex) => (
+        <div
+          key={phase.id}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+            marginBottom: "3rem",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: "1rem",
+            }}
+          >
+            <div
+              style={{
+                borderRadius: "50%",
+                width: "3rem",
+                height: "3rem",
+                backgroundColor: "black",
+                color: "white",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: "1.5rem",
+                marginRight: "1rem",
+              }}
+            >
+              {phaseIndex + 1}
+            </div>
+            <Typography variant="h4">
+              {phase.name}
+              {phase.tasks.every((task) => task.completed) && " ✔️"}
+            </Typography>
+          </div>
+          {phase.tasks.map((task, taskIndex) => (
             <div key={task.id}>
               <FormControlLabel
                 control={
                   <Checkbox
                     checked={task.completed}
-                    onChange={() => handleTaskToggle(phase.id, task.id)}
+                    onChange={() => handleTaskToggle(phaseIndex, taskIndex)}
                     color="primary"
-                    disabled={!canMarkTask(phase.id, task.id)}
+                    disabled={!canMarkTask(phaseIndex, taskIndex)}
                   />
                 }
                 label={task.description}
@@ -121,6 +210,27 @@ const Home: React.FC = () => {
           ))}
         </div>
       ))}
+      {areAllPhasesCompleted() && (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleCompleteAllPhases}
+          style={{ marginTop: "1rem" }}
+        >
+          Complete All Phases
+        </Button>
+      )}
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>Random Fact</DialogTitle>
+        <DialogContent>
+          <Typography>{randomFact}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
